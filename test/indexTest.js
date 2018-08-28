ae7afd02727fb2f278def3c1406e6ef665eaaff3
ritz-w <ritz.wu@outlook.com>
2018-08-28 12:14:41 +0100

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -13,7 +13,7 @@ describe('Fix the Scope', function() {
 
       upperCaseCustomerName();
 
-      expect(window.customerName).to.equal('BOB');
+      expect(window.customerName).to.equal("BOB");
     });
   });
   describe('setBestCustomer()', function() {
